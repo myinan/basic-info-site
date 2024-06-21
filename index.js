426f9fad423a8522bad8de4f73b767812cba7ceb
myinan <m.yasir.inan@gmail.com>
2024-06-22 00:15:19 +0300

    Serve html files

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 import http from "node:http";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-const server = http.createServer();
+const server = http.createServer(async (req, res) => {
+  const { url } = req;
 
-server.listen(8080);
-
-server.on("request", (req, res) => {
-  const { headers, method, url } = req;
-
-  req.on("error", (err) => {
+  try {
+    switch (url) {
+      case "/":
+        await serveFile(res, "views/index.html");
+        break;
+      case "/about":
+        await serveFile(res, "views/about.html");
+        break;
+      case "/contact-me":
+        await serveFile(res, "views/contact-me.html");
+        break;
+      default:
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/html");
+        res.end("<h1>404 Not Found</h1>");
+    }
+  } catch (err) {
     console.error(err);
-    res.statusCode = 400;
-    res.end("Bad Request");
-  });
-
-  res.on("error", (err) => {
-    console.error(err);
-  });
-
-  res.setHeader("Content-Type", "text/plain");
-
-  switch (url) {
-    case "/":
-      res.end("Welcome to the index page.");
-      break;
-    case "/about":
-      res.end("About us...");
-      break;
-    case "/contact-me":
-      res.end("Contact me...");
-      break;
-    default:
-      res.statusCode = 404;
-      res.end("404 Not Found");
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>500 Internal Server Error</h1>");
   }
 });
 
-console.log("Server running at http://localhost:8080/");
+async function serveFile(res, filePath) {
+  const fullPath = join(process.cwd(), filePath);
+  const data = await readFile(fullPath, "utf-8");
+  res.setHeader("Content-Type", "text/html");
+  res.end(data);
+}
+
+server.listen(8080, () => {
+  console.log("Server running at http://localhost:8080/");
+});
